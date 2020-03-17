c52c24b7060e92931a7b74e9004fbd44aaa2ef8a
Kvitebjornen <alxhansen@gmail.com>
2020-03-17 19:05:01 +0100

    jpa added to UserRepository

--- src/main/java/com/example/demo/PetMatchController.java
+++ src/main/java/com/example/demo/PetMatchController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ public class PetMatchController {
     //Ny bruker Controller
 
     @GetMapping("/registrer")
-    public String getRegistrer() {
+    public String getRegistrer(@ModelAttribute User user) {
         return "nyBruker";
     }
 
