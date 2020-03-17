e90e3fb67ae965093fc6a4d1938df17116b1a27e
Angela <angela.jakobsen@gmail.com>
2020-03-17 16:27:03 +0100

    Added html and css for ny bruker, added get and post for ny bruker

--- src/main/java/com/example/demo/PetMatchController.java
+++ src/main/java/com/example/demo/PetMatchController.java
@@ -1,8 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PetMatchController {
+
+    @Autowired UserRepository userRepository;
+
+    //Ny bruker Controller
+
+    @GetMapping("/registrer")
+    public String getRegistrer() {
+        return "nyBruker";
+    }
+
+    @PostMapping("/registrer")
+    public String postRegistrer(@ModelAttribute User user) {
+
+        userRepository.save(user);
+
+        return "intropage";
+    }
 
 }
