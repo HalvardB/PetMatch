67b0b96491d9ba6a3a648bc82dbcad8388d77876
Kvitebjornen <alxhansen@gmail.com>
2020-03-20 14:47:39 +0100

    Updated user login

--- src/main/java/com/example/demo/Seller.java
+++ src/main/java/com/example/demo/Seller.java
@@ -6,7 +6,7 @@ public class Seller extends User {
     List<Animal> animalList;
 
     public Seller(String firstName, String lastName, String password, String email, Municipality municipality, String bio, Boolean wantNewsletter, UserType userType, String userImage, AnimalType animalType, List<Animal> animalList) {
-        super(firstName, lastName, password, email, municipality, bio, wantNewsletter, userType, userImage);
+        super(firstName, lastName, password, email, municipality, bio, wantNewsletter, userType);
         this.animalList = animalList;
     }
 }
