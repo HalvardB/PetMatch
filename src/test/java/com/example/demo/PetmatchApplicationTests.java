99227426fb9135cc9e9c99d12b944caa346a3c14
Halvard Bastiansen <Halvard.bastiansen@gmail.com>
2020-03-22 20:12:42 +0100

    Added unit tests for methods

--- src/test/java/com/example/demo/PetmatchApplicationTests.java
+++ src/test/java/com/example/demo/PetmatchApplicationTests.java
@@ -4,35 +4,53 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PetmatchApplicationTests {
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    MatchRepository matchRepository;
+    PetMatchController pmc = new PetMatchController();
 
     @Test
     void contextLoads() {
     }
 
     @Test
-    void canLoadEmptyH2db(){
-        assertEquals(true, userRepository.findAll().iterator().hasNext());
+    void testLoadH2db(){
+        assertEquals(true, pmc.userRepository.findAll().iterator().hasNext());
     }
 
     @Test
-    void makeAUserProfile() {
+    void testCreateUser() {
         User user = new User("Robin", "Dalboy", "Secret", "robin@dalboy.com");
-        userRepository.save(user);
-        assertEquals(true, userRepository.findAll().iterator().hasNext());
+        pmc.userRepository.save(user);
+        assertEquals("Robin", pmc.userRepository.findByEmail("robin@dalboy.com").getFirstName());
     }
 
     @Test
-    void testMatches() {
+    void testGetAnimalsFromMatches(){
+        List<Match> matchList = pmc.matchRepository.findAllByUserId(1);
+        List<Animal> animalList = pmc.getAnimalsFromMatches(matchList);
+        assertEquals(2, animalList.size());
+    }
 
+    @Test
+    void testGetUsersFromMatches() {
+        List<Match> matchList = pmc.matchRepository.findAllByAnimalId(13);
+        List<User> userList = pmc.getUsersFromMatches(matchList);
+        assertEquals(3, userList.size());
+    }
+
+    @Test
+    void testApproveMatch(){
+        Match match = pmc.matchRepository.findById(1).get();
+        assertEquals(false, match.getApproved());
+        pmc.approveMatch(1);
+
+        match = pmc.matchRepository.findById(1).get();
+        assertEquals(true, match.getApproved());
     }
 }
