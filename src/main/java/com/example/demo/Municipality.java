30f653d6fda0379c6fb9a997b7c672ff018566a3
Robin D <robin.dalbak@gmail.com>
2020-03-25 15:38:23 +0100

    Changed enum on size and Municipality

--- src/main/java/com/example/demo/Municipality.java
+++ src/main/java/com/example/demo/Municipality.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
 public enum Municipality {
-    AGDER,
-    INNLANDET,
-    MORE_OG_ROMSDAL,
-    NORDLAND,
-    OSLO,
-    ROGALAND,
-    VESTFOLD_OG_TELEMARK,
-    TROMS_OG_FINNMARK,
-    TRONDELAG,
-    VESTLAND,
-    VIKEN
+    AGDER("Agder"),
+    INNLANDET("Innlandet"),
+    MORE_OG_ROMSDAL("Møre og Romsdal"),
+    NORDLAND("Nordland"),
+    OSLO("Oslo"),
+    ROGALAND("Rogaland"),
+    VESTFOLD_OG_TELEMARK("Vestfold og Telemark"),
+    TROMS_OG_FINNMARK("Troms og Finnmark"),
+    TRONDELAG("Trøndelag"),
+    VESTLAND("Vestland"),
+    VIKEN("Viken");
 
+    private final String displayValue;
+
+    private Municipality(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
